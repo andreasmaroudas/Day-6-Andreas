20f161d1b7dac2d9d5d8e4cb94cb56b84e8972ef
ihope <ihope@DESKTOP-573JUSM>
2024-05-25 23:22:46 +0300

    Add project files.

---
+++ ArenaGameEngine/Necromancer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGameEngine
+{
+    public class Necromancer : Hero
+    {
+        public bool Imune { get; set; }
+
+        public Necromancer() : base("Necromancer")
+        {
+        }
+        public Necromancer(string name) : base(name)
+        {
+        }
+        public override void TakeDamage(int incomingDamage)
+        {
+            int n = Random.Shared.Next(0, 100);
+            if (n <= 50 || Imune == true)
+            {
+                incomingDamage = 0;
+            }
+            base.TakeDamage(incomingDamage);
+        }
+
+        public override int Attack()
+        {
+            int drainLife = base.Attack();
+
+
+            int dice = Random.Shared.Next(0, 100);
+            if (dice <= 20)
+            {
+                drainLife = Strength * 3;
+
+            }
+            return drainLife;
+        }
+
+
+        public override int GainHealth()
+        {
+            int gainLife = base.GainHealth();
+            int dice2 = Random.Shared.Next(0, 100);
+            if (dice2 <= 10)
+            {
+                gainLife = Health + 100;
+            }
+            return gainLife;
+        }
+    }
+}
---
+++ ArenaGameEngine/Warrior.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGameEngine
+{
+    public class Warrior : Hero
+    {
+        public Warrior() : base("Warrior")
+        {
+        }
+        public Warrior(string name) : base(name)
+        {
+        }
+
+
+
+        public override int Attack()
+        {
+            int baseAttack = base.Attack();
+
+            if (Health <= 200)
+            {
+                baseAttack = Strength * 5;
+            }
+            return baseAttack;
+        }
+        public override void TakeDamage(int incomingDamage)
+        {
+            int dice = Random.Shared.Next(0, 100);
+            if (dice <= 20)
+            {
+                incomingDamage = 0;
+            }
+            base.TakeDamage(incomingDamage);
+        }
+    }
+}
